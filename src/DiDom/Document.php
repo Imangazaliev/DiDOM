568272472a6034cd43ad7519ae378b8f16bc7a8e
Atef Ben Ali <atefBB@users.noreply.github.com>
2017-01-26 23:21:28 +0100

    add '@' after file_get_contents() function
    add '@' after 'file_get_contents()' PHP function to avoid that this function throw the 'normal' exception (see: http://php.net/manual/en/function.file-get-contents.php).

--- src/DiDom/Document.php
+++ src/DiDom/Document.php
@@ -296,7 +296,7 @@ class Document
             }
         }
 
-        $content = file_get_contents($filepath);
+        $content = @file_get_contents($filepath);
 
         if ($content === false) {
             throw new RuntimeException(sprintf('Could not load file %s', $filepath));
